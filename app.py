cae9fddc1efe334f41f4cf8fcda61ad57e3eabff
DUGORN <166985056+DUGORN@users.noreply.github.com>
2026-06-27 02:19:36 +0700

    Update app.py

--- app.py
+++ app.py
@@ -11,9 +11,8 @@ st.set_page_config(
     page_title="V16.1 Scanner",
     page_icon="🚀",
     layout="centered",
-    initial_sidebar_state="collapsed"
+    initial_sidebar_state="collapsed"  # ✅ เปลี่ยนเป็น "collapsed"
 )
-
 # ============================================================================
 #  ไฟล์เก็บข้อมูล
 # ============================================================================
